cebf936df260d048bb38704d30afe15c09821b5e
HgaMen <hga.int2021@gmail.com>
2022-10-11 13:42:38 -0500

    dia 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import IndexHeader from '@/components/header/IndexHeader';
-import IndexProduct from '@/components/product/IndexProduct';
+import IndexHeader from "@/components/header/IndexHeader";
+import IndexProduct from "@/components/product/IndexProduct";
+
+import CartDetailsProvider from "@/context/useCartDetails"; 
 
 const App = () => {
-  return (
-    <>
-      <IndexHeader />
-      <IndexProduct />
-    </>
-  );
+    return (
+        <CartDetailsProvider>
+            <IndexHeader />
+            <IndexProduct />
+        </CartDetailsProvider>
+    );
 };
 
 export default App;
